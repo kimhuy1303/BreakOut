4261314803bea05d6e709f3642c16b49b616123d
Kim Huy <52100964@student.tdtu.edu.vn>
2022-12-06 20:14:57 +0700

    Update input placeholder and file register

--- animate.js
+++ animate.js
@@ -2,10 +2,11 @@ var popUp = document.querySelector("#setting-pop-up");
 // var menu = document.querySelector("#menu-start-game");
 var blur = document.querySelector("#blurBackground");
 var text = document.querySelector("#text");
-function settingPopUp(){
-  popUp.style.display ="block";
+function settingPopUp() {
+  popUp.style.display = "block";
   // menu.style.backgroundColor = rgba(0, 0, 0, 0.235);
   blur.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
   text.style.color = "#fefcfea2";
-  text.style.textShadow = "2px 0 0 #e02a4f, -2px 0 0 #e02a4f, 0 2px 0 #e02a4f, 0 -2px 0 #e02a4f, 1px 1px #e02a4f, -1px -1px 0 #e02a4f, 1px -1px 0 #e02a4f, -1px 1px 0 #e02a4f";
+  text.style.textShadow =
+    "2px 0 0 #e02a4f, -2px 0 0 #e02a4f, 0 2px 0 #e02a4f, 0 -2px 0 #e02a4f, 1px 1px #e02a4f, -1px -1px 0 #e02a4f, 1px -1px 0 #e02a4f, -1px 1px 0 #e02a4f";
 }
