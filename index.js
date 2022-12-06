dacecc75a7e580c82ac223b7a5fc50ae2035cd80
Quy Vy <quyvylehoang3003@gmail.com>
2022-12-06 19:46:13 +0700

    Update animate.js

--- index.js
+++ index.js
@@ -327,14 +327,4 @@ function loop() {
     moveBall();
   }
 }
-var popUp = document.querySelector("#setting-pop-up");
-// var menu = document.querySelector("#menu-start-game");
-var blur = document.querySelector("#blurBackground");
-var text = document.querySelector("#text");
-function settingPopUp(){
-  popUp.style.display ="block";
-  // menu.style.backgroundColor = rgba(0, 0, 0, 0.235);
-  blur.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  text.style.color = "#fefcfea2";
-  text.style.textShadow = "2px 0 0 #e02a4f, -2px 0 0 #e02a4f, 0 2px 0 #e02a4f, 0 -2px 0 #e02a4f, 1px 1px #e02a4f, -1px -1px 0 #e02a4f, 1px -1px 0 #e02a4f, -1px 1px 0 #e02a4f";
-}
+
